7195a0593b96bc444f796dbfbc062baab3c62e9c
Chandler Teigen <chandlerteigen@gmail.com>
2019-09-20 22:43:42 -0700

    add comments

--- PA2/src/Timer.cpp
+++ PA2/src/Timer.cpp
@@ -7,22 +7,22 @@ Timer::~Timer(){}
 
 std::chrono::nanoseconds Timer::getLength()
 {
-    return length;
+    return length; // return length of elapsed time in nanoseconds
 }
 
 void Timer::start()
 {
-    startTime = std::chrono::high_resolution_clock::now();
+    startTime = std::chrono::high_resolution_clock::now(); // records current time as starting time
 }
 void Timer::stop()
 {
-    stopTime = std::chrono::high_resolution_clock::now();
-    length = std::chrono::duration_cast<std::chrono::nanoseconds> (stopTime - startTime);
+    stopTime = std::chrono::high_resolution_clock::now(); // records current time as ending time
+    length = std::chrono::duration_cast<std::chrono::nanoseconds> (stopTime - startTime); // calculate difference between stop and start
     
 
 }
 
 void Timer::printTime()
 {
-    std::cout << length.count() / 1000000 << "ms" << std::endl;
+    std::cout << length.count() / 1000000 << "ms" << std::endl; // print time in milliseconds
 }
